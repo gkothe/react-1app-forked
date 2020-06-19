5be3237bc195e56e794a23f4a8e363e590fe5c37
gkothe <g.kothe@hotmail.com>
2020-06-19 10:08:23 -0300

    nova tentiva

--- index.js
+++ index.js
@@ -1,73 +1,174 @@
-import React from "react";
-import PhotosUpload from "./src/PhotosUpload";
-import Photos from "./src/Photos";
-import Image from "./src/Image";
-import ImageUpload from "./src/ImageUpload";
-import Text from "./src/Text";
-import ListView from "./src/ListView";
-import GridView from "./src/GridView";
-import Modal from "./src/Modal";
-import Alert from "./src/Alert";
-import Content from "./src/Content";
-import InitApp from "./src/InitApp";
-import SelectInput from "./src/SelectInput";
-import RefreshControl from "./src/RefreshControl";
-import Switch from "./src/Switch";
-import View from "./src/View";
-import Table from "./src/Table";
-import TouchableOpacity from "./src/TouchableOpacity";
-import FlatList from "./src/FlatList";
-import ScrollView from "./src/ScrollView";
-import TextInput from "./src/TextInput";
-import Progress from "./src/Progress";
-import Icon from "./src/Icon";
-import Fragment from "./src/Fragment";
-import TitleBar from "./src/TitleBar";
-import { styleMack } from "./src/Util";
-import File from "./src/File";
-import StyleSheet from "./src/StyleSheet";
-import DatePicker from "./src/DatePicker";
-import TimePicker from "./src/TimePicker";
-import "./src/styles.css";
-import { OpenModal, CloseModal } from "./src/PgModal";
-import { OpenSnack } from "./src/SnackBar";
-import { OpenDialog, CloseDialog } from "./src/DialogAlert";
-import { OpenDialogOption, CloseDialogOption } from "./src/DialogOptions";
+"use strict";
 
-export {
-  PhotosUpload,
-  OpenDialogOption,
-  CloseDialogOption,
-  OpenDialog,
-  CloseDialog,
-  OpenModal,
-  CloseModal,
-  OpenSnack,
-  Photos,
-  Image,
-  ImageUpload,
-  Text,
-  ListView,
-  GridView,
-  InitApp,
-  StyleSheet,
-  RefreshControl,
-  TouchableOpacity,
-  ScrollView,
-  TextInput,
-  TitleBar,
-  Progress,
-  Switch,
-  View,
-  Fragment,
-  Icon,
-  SelectInput,
-  Content,
-  Modal,
-  Alert,
-  Table,
-  File,
-  DatePicker,
-  TimePicker,
-  FlatList
-};
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.FlatList = exports.TimePicker = exports.DatePicker = exports.File = exports.Table = exports.Alert = exports.Modal = exports.Content = exports.SelectInput = exports.Icon = exports.Fragment = exports.View = exports.Switch = exports.Progress = exports.TitleBar = exports.TextInput = exports.ScrollView = exports.TouchableOpacity = exports.RefreshControl = exports.StyleSheet = exports.InitApp = exports.GridView = exports.ListView = exports.Text = exports.ImageUpload = exports.Image = exports.Photos = exports.OpenSnack = exports.CloseModal = exports.OpenModal = exports.CloseDialog = exports.OpenDialog = exports.CloseDialogOption = exports.OpenDialogOption = exports.PhotosUpload = undefined;
+
+var _react = require("react");
+
+var _react2 = _interopRequireDefault(_react);
+
+var _PhotosUpload = require("./lib/PhotosUpload");
+
+var _PhotosUpload2 = _interopRequireDefault(_PhotosUpload);
+
+var _Photos = require("./lib/Photos");
+
+var _Photos2 = _interopRequireDefault(_Photos);
+
+var _Image = require("./lib/Image");
+
+var _Image2 = _interopRequireDefault(_Image);
+
+var _ImageUpload = require("./lib/ImageUpload");
+
+var _ImageUpload2 = _interopRequireDefault(_ImageUpload);
+
+var _Text = require("./lib/Text");
+
+var _Text2 = _interopRequireDefault(_Text);
+
+var _ListView = require("./lib/ListView");
+
+var _ListView2 = _interopRequireDefault(_ListView);
+
+var _GridView = require("./lib/GridView");
+
+var _GridView2 = _interopRequireDefault(_GridView);
+
+var _Modal = require("./lib/Modal");
+
+var _Modal2 = _interopRequireDefault(_Modal);
+
+var _Alert = require("./lib/Alert");
+
+var _Alert2 = _interopRequireDefault(_Alert);
+
+var _Content = require("./lib/Content");
+
+var _Content2 = _interopRequireDefault(_Content);
+
+var _InitApp = require("./lib/InitApp");
+
+var _InitApp2 = _interopRequireDefault(_InitApp);
+
+var _SelectInput = require("./lib/SelectInput");
+
+var _SelectInput2 = _interopRequireDefault(_SelectInput);
+
+var _RefreshControl = require("./lib/RefreshControl");
+
+var _RefreshControl2 = _interopRequireDefault(_RefreshControl);
+
+var _Switch = require("./lib/Switch");
+
+var _Switch2 = _interopRequireDefault(_Switch);
+
+var _View = require("./lib/View");
+
+var _View2 = _interopRequireDefault(_View);
+
+var _Table = require("./lib/Table");
+
+var _Table2 = _interopRequireDefault(_Table);
+
+var _TouchableOpacity = require("./lib/TouchableOpacity");
+
+var _TouchableOpacity2 = _interopRequireDefault(_TouchableOpacity);
+
+var _FlatList = require("./lib/FlatList");
+
+var _FlatList2 = _interopRequireDefault(_FlatList);
+
+var _ScrollView = require("./lib/ScrollView");
+
+var _ScrollView2 = _interopRequireDefault(_ScrollView);
+
+var _TextInput = require("./lib/TextInput");
+
+var _TextInput2 = _interopRequireDefault(_TextInput);
+
+var _Progress = require("./lib/Progress");
+
+var _Progress2 = _interopRequireDefault(_Progress);
+
+var _Icon = require("./lib/Icon");
+
+var _Icon2 = _interopRequireDefault(_Icon);
+
+var _Fragment = require("./lib/Fragment");
+
+var _Fragment2 = _interopRequireDefault(_Fragment);
+
+var _TitleBar = require("./lib/TitleBar");
+
+var _TitleBar2 = _interopRequireDefault(_TitleBar);
+
+var _Util = require("./lib/Util");
+
+var _File = require("./lib/File");
+
+var _File2 = _interopRequireDefault(_File);
+
+var _StyleSheet = require("./lib/StyleSheet");
+
+var _StyleSheet2 = _interopRequireDefault(_StyleSheet);
+
+var _DatePicker = require("./lib/DatePicker");
+
+var _DatePicker2 = _interopRequireDefault(_DatePicker);
+
+var _TimePicker = require("./lib/TimePicker");
+
+var _TimePicker2 = _interopRequireDefault(_TimePicker);
+
+require("./lib/styles.css");
+
+var _PgModal = require("./lib/PgModal");
+
+var _SnackBar = require("./lib/SnackBar");
+
+var _DialogAlert = require("./lib/DialogAlert");
+
+var _DialogOptions = require("./lib/DialogOptions");
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+// var Component = React.Component;
+
+exports.PhotosUpload = _PhotosUpload2.default;
+exports.OpenDialogOption = _DialogOptions.OpenDialogOption;
+exports.CloseDialogOption = _DialogOptions.CloseDialogOption;
+exports.OpenDialog = _DialogAlert.OpenDialog;
+exports.CloseDialog = _DialogAlert.CloseDialog;
+exports.OpenModal = _PgModal.OpenModal;
+exports.CloseModal = _PgModal.CloseModal;
+exports.OpenSnack = _SnackBar.OpenSnack;
+exports.Photos = _Photos2.default;
+exports.Image = _Image2.default;
+exports.ImageUpload = _ImageUpload2.default;
+exports.Text = _Text2.default;
+exports.ListView = _ListView2.default;
+exports.GridView = _GridView2.default;
+exports.InitApp = _InitApp2.default;
+exports.StyleSheet = _StyleSheet2.default;
+exports.RefreshControl = _RefreshControl2.default;
+exports.TouchableOpacity = _TouchableOpacity2.default;
+exports.ScrollView = _ScrollView2.default;
+exports.TextInput = _TextInput2.default;
+exports.TitleBar = _TitleBar2.default;
+exports.Progress = _Progress2.default;
+exports.Switch = _Switch2.default;
+exports.View = _View2.default;
+exports.Fragment = _Fragment2.default;
+exports.Icon = _Icon2.default;
+exports.SelectInput = _SelectInput2.default;
+exports.Content = _Content2.default;
+exports.Modal = _Modal2.default;
+exports.Alert = _Alert2.default;
+exports.Table = _Table2.default;
+exports.File = _File2.default;
+exports.DatePicker = _DatePicker2.default;
+exports.TimePicker = _TimePicker2.default;
+exports.FlatList = _FlatList2.default;
---
+++ indexEdit.js
@@ -0,0 +1,79 @@
+import React from "react";
+// var Component = React.Component;
+
+import PhotosUpload from "./lib/PhotosUpload";
+import Photos from "./lib/Photos";
+import Image from "./lib/Image";
+import ImageUpload from "./lib/ImageUpload";
+import Text from "./lib/Text";
+import ListView from "./lib/ListView";
+import GridView from "./lib/GridView";
+
+import Modal from "./lib/Modal";
+import Alert from "./lib/Alert";
+import Content from "./lib/Content";
+import InitApp from "./lib/InitApp";
+
+import SelectInput from "./lib/SelectInput";
+import RefreshControl from "./lib/RefreshControl";
+import Switch from "./lib/Switch";
+import View from "./lib/View";
+import Table from "./lib/Table";
+
+import TouchableOpacity from "./lib/TouchableOpacity";
+import FlatList from "./lib/FlatList";
+import ScrollView from "./lib/ScrollView";
+import TextInput from "./lib/TextInput";
+import Progress from "./lib/Progress";
+import Icon from "./lib/Icon";
+import Fragment from "./lib/Fragment";
+import TitleBar from "./lib/TitleBar";
+import { styleMack } from "./lib/Util";
+import File from "./lib/File";
+import StyleSheet from "./lib/StyleSheet";
+import DatePicker from "./lib/DatePicker";
+import TimePicker from "./lib/TimePicker";
+import "./lib/styles.css";
+
+import { OpenModal, CloseModal } from "./lib/PgModal";
+import { OpenSnack } from "./lib/SnackBar";
+import { OpenDialog, CloseDialog } from "./lib/DialogAlert";
+import { OpenDialogOption, CloseDialogOption } from "./lib/DialogOptions";
+
+export {
+  PhotosUpload,
+  OpenDialogOption,
+  CloseDialogOption,
+  OpenDialog,
+  CloseDialog,
+  OpenModal,
+  CloseModal,
+  OpenSnack,
+  Photos,
+  Image,
+  ImageUpload,
+  Text,
+  ListView,
+  GridView,
+  InitApp,
+  StyleSheet,
+  RefreshControl,
+  TouchableOpacity,
+  ScrollView,
+  TextInput,
+  TitleBar,
+  Progress,
+  Switch,
+  View,
+  Fragment,
+  Icon,
+  SelectInput,
+  Content,
+  Modal,
+  Alert,
+  Table,
+  File,
+  DatePicker,
+  TimePicker,
+  FlatList
+};
